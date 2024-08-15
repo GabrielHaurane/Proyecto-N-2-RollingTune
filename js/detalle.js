20c1a32eadc42125d00fe602d28155eedaa8f3aa
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-08-15 19:49:27 -0300

    creacion del navBar.js y navBar.css para todas las paginas

--- js/detalle.js
+++ js/detalle.js
@@ -1,8 +1,2 @@
-window.addEventListener('scroll', function() { 
-    const navbar = document.getElementById('navbar'); 
-    if (window.scrollY > 50) { 
-    navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    } else { 
-    navbar.style.backgroundColor = 'rgba(0, 0, 0, 1)';
-    } 
-    });
+import { funcionScroll } from "./navbar.js";
+funcionScroll();
