b7af61f9710a41a4365e28cd21b98eeed64fee25
Gustavo Gettar <gus.gettar@gmail.com>
2024-08-21 20:19:09 -0300

    agregue modificar y eliminar canciones

--- js/claseCancion.js
+++ js/claseCancion.js
@@ -1,6 +1,9 @@
 //clase cancion
- export class cancion {
-    constructor(nombre,artista,album,fecha,categoria,duracion,audio,imgcancion){
+
+import { v4 as uid } from "https://jspm.dev/uuid";
+export class cancion {
+    constructor(id,nombre,artista,album,fecha,categoria,duracion,audio,imgcancion){
+        this.id=uid()
         this.nombre=nombre
         this.artista=artista
         this.album=album
