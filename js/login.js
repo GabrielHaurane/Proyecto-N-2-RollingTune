a9cb43bc708a8d89785df3ee424a879e7f81fb41
Berenice Contreras <contbere@gmail.com>
2024-08-24 14:04:24 -0300

    Hice una ventana modal con el login y las validaciones en js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,25 @@
 
+function loguear() {
+    const validEmail = "bere@gmail.com";
+    const validPassword = "12345678";
+
+    const userEmail = document.getElementById("InputEmail").value.trim();
+    const userPassword = document.getElementById("InputPassword1").value.trim();
+
+    if (!validateEmailFormat(userEmail)) {
+        alert("Formato de correo electrónico incorrecto");
+        return;
+    }
+
+    if (userEmail === validEmail && userPassword === validPassword) {
+        alert("Bienvenido");
+        // Redirigir al usuario o realizar otra acción aquí
+    } else {
+        alert("Correo electrónico o contraseña incorrectos");
+    }
+}
+
+function validateEmailFormat(email) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+}
