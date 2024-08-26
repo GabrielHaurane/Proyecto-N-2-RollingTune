158661cdd21c9341e00f696d39f733289127468c
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-08-25 22:48:05 -0300

    limpieza de codigo

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,8 +1,6 @@
 import { funcionScroll } from "./navbar.js";
 funcionScroll;
-import { loguear, validateEmailFormat } from "./login.js";
-loguear;
-validateEmailFormat;
+
   const playButton1 = document.getElementById('play-button-1');
   const audio1 = document.getElementById('audio-1');
   const playButton2 = document.getElementById('play-button-2');
