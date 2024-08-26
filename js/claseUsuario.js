035772f7f9e479184bc267b14347bb60d15576ca
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-08-26 07:00:45 -0300

    limpieza del codigo de la rama inicioSesionB

--- js/claseUsuario.js
+++
@@ -1,8 +0,0 @@
-export class Usuario {
-    constructor(nombre, email, contraseña) {
-      this.nombre = nombre;
-      this.email = email;
-      this.contraseña = contraseña;
-      this.isAdmin = false;
-    }
-  }
